d455a83d773a440b1022d50806ea11cf1269f25b
zsxneil <zsxneil@hotmail.com>
2017-04-06 17:34:27 +0800

    log4j打印出sql

--- ssm/src/test/java/com/my/ssm/demo/TestMyBatis.java
+++ ssm/src/test/java/com/my/ssm/demo/TestMyBatis.java
@@ -16,7 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.my.ssm.demo.model.User;
 import com.my.ssm.demo.service.IUserService;
 
-@RunWith(SpringJUnit4ClassRunner.class) // 表示继承了SpringJUnit4ClassRunner类
+//@RunWith(SpringJUnit4ClassRunner.class) // 表示继承了SpringJUnit4ClassRunner类
 @ContextConfiguration(locations = { "classpath:spring-mybatis.xml" })
 public class TestMyBatis {
 	
